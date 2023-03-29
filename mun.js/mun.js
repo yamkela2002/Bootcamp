7a1907515d850c4b5767af3dc0ff925fa99a683b
Yamkela Ngwetsheni <yamkela.ngwetsheni@gmail.com>
2023-03-29 16:09:55 +0200

    firth commit

---
+++ mun.js/mun.js
@@ -0,0 +1,30 @@
+/*var a=1;
+var a=10;
+let=
+let a=10;
+console.log(a)
+*/
+/*let  a=10
+if(a>10){
+    console.log("Greater than a decade");
+}else if (a==10){
+    console.log("it is a decade")
+
+}else if(a<10||a>=0){
+   console.log("it is lesser than a decade") 
+}else{
+    console.log("we cannot have negative years")
+}*/
+
+let a= 70;
+let b= 80;
+let c= 90;
+
+if (c>a) {
+    console.log("c is the maximum value")
+}else if (c<a && c<b){
+    console.log("False")
+
+}
+
+
