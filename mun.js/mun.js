a14a7f75e9c733b302e0367a1bbf179179ceed94
Yamkela Ngwetsheni <yamkela.ngwetsheni@gmail.com>
2023-03-30 13:30:43 +0200

    sixth commit

--- mun.js/mun.js
+++ mun.js/mun.js
@@ -16,15 +16,38 @@ if(a>10){
     console.log("we cannot have negative years")
 }*/
 
-let a= 70;
+/*let a= 70;
 let b= 80;
 let c= 90;
 
-if (c>a) {
+if (c>a && c>b) {
     console.log("c is the maximum value")
 }else if (c<a && c<b){
     console.log("False")
+}
+*/
 
+
+
+/*let e = 10;
+
+if (e % 2===0){
+    console.log("even number")
+}else if (e % 2=== 1){
+    console.log("odd number")
+}
+*/
+
+function CheckEvenNumber(){
+
+let number = document.getElementById("evennumber").value
+
+if(number%2===0){
+    alert(number + " "+"is an even number")
+}
+else{
+    alert(number +" "+"is an odd number")
+}
 }
 
 
