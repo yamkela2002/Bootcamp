627aa8fa0874454cb540f0e1a6476550f239a797
Yamkela Ngwetsheni <yamkela.ngwetsheni@gmail.com>
2023-03-31 15:28:40 +0200

    seventh commit

--- mun.js/Array.js
+++ mun.js/Array.js
@@ -1,11 +1,37 @@
 
-let numbers = [5,6,7,8,3,9,4,2]
-numbers.pop()
-numbers.push(10)
+// let numbers = [5,6,7,8,3,9,4,2]
+// numbers.pop()
+// numbers.push(10)
 
-numbers.shift()
-numbers.unshift(29)
+// numbers.shift()
+// numbers.unshift(29)
+
+// console.log(numbers)
 
 
-console.log(numbers)
 
+
+let numbers =[20,311,24,15,26,49,66,89,]
+
+ let oddnumbers = [];
+ let evennumbers = [];
+
+ for(let y=0; y<numbers.length; y++)
+ if(numbers[y]%2==0)
+
+ {
+    oddnumbers.push(numbers[y])
+ }
+else{
+    evennumbers.push(numbers[y])
+}
+console.log(evennumbers)
+console.log(oddnumbers)
+
+
+
+ 
+ 
+
+
+ 
