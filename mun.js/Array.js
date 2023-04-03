8c72efc3d5c6a1f4cbb9f76db84997444bc0c268
Yamkela Ngwetsheni <yamkela.ngwetsheni@gmail.com>
2023-04-03 16:12:43 +0200

    eigth commit

--- mun.js/Array.js
+++ mun.js/Array.js
@@ -11,7 +11,7 @@
 
 
 
-let numbers =[20,311,24,15,26,49,66,89,]
+/*let numbers =[20,311,24,15,26,49,66,89,]
 
  let oddnumbers = [];
  let evennumbers = [];
@@ -23,15 +23,89 @@ let numbers =[20,311,24,15,26,49,66,89,]
     oddnumbers.push(numbers[y])
  }
 else{
-    evennumbers.push(numbers[y])
-}
-console.log(evennumbers)
-console.log(oddnumbers)
+//     evennumbers.push(numbers[y])
+// }
+// console.log(evennumbers)
+// console.log(oddnumbers)*/
+
+// // let numbers=[-3,-100,10,4,6,7,20,1]
+
+// //  let maximumnumber= numbers[numbers.length-1]
+// //  let minimnumber=[numbers]
+
+// //  numbers.sort((a,b)=>a-b)
+// //  console.log
+
+// //let letters=["b","c","a","l","k","r","g","d"]
+// //numbers.sort((ab,b)=>a-b)
+// //console.log(letters)
+
+// function reversed(name){
+
+// let reversed="";
+// for(let y = 0; y >=10.length; y--) {
+//     reversed=text[y] + reversed;
+// }
+// console.log(reversed);
+// }
+// reversedname= ("i am testing")
+// reversedname=();
+
+// // let myName="Yamkela"
+// // let letter=[];
 
 
 
- 
- 
+// function maxAndMin (numbers){
+//     let obj = []
+//     numbers.sort((a,b)=>a-b)
+//     obj["minNumber"]=numbers[0]
+//     obj["maxNumber"]=numbers[numbers.length-1]
+//     return obj
+// }
+// maxAndMin([3,8,9,10,11,12,13,14,16])
+// let firstApp = maxAndMin([3,8,9.10,11,12,13,14,16])
+// console.log (firstApp)
 
 
- 
+
+// function calculate salary (hours,h,days){
+//     let salary=hours*days*
+//     console.log(salary)
+// }
+// calculate salary(32*7*20)
+
+// function calculate (hr,h,days,pci=100){
+//     return hr*h*d
+// }
+// console.log (32*7)
+
+
+
+// function emailGen(fname +"yami"+ yname ="tawanda") {
+//     return fname+"."+yname+"@gsb.co.za"
+// }
+// console.log(emailGen())
+
+
+// function emailGen(fname="joe"+ sname "") {
+//     let newEmail=fname+"."+iname+"@gsb.co.za"
+//     return newEmail.toLowercase();
+// }
+// console.log(emailGen fname=,"yamkela ngwetsheni"())
+
+
+
+
+
+
+
+
+
+
+let num1=[1,3,5,7]
+let num2=[0,2,4,6]
+let num3=[...num1,...num2]
+let num4=[num1.concat(num2)]
+console.log("This is num3",num3)
+console.log("This is num4",num4)
