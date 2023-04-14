e81173826d742ab117163704217e5925d55affc7
Yamkela Ngwetsheni <yamkela.ngwetsheni@gmail.com>
2023-04-14 16:51:45 +0200

    elenth commit

--- Temperature convert/convert.js
+++ Temperature convert/convert.js
@@ -7,14 +7,14 @@ let C=(document.getElementById("Fahrenheits").value)
    let f=(C*9/5+32)
 
     if(f<16){
-        alert(f+""+"it is cold,wear a jacket");
+        alert(f + "" + "it is cold,wear a jacket");
     }   
     else if (f<=16 && f<=21){
-       alert(f+""+"if is warm,wear a t-shirt and jeans");
+       alert(f + "" + "if is warm,wear a t-shirt and jeans");
     }
     else if (f>21){
 
-   alert (f+""+"it is hot,wear shorts");
+   alert (f + "" + "it is hot,wear shorts");
      }
     }
 
--- image slider/sliders.js
+++ image slider/sliders.js
@@ -1,41 +1,53 @@
 
-
+let show_images = document.querySelector(".show_image")
 
 //Array where pictures stored
 let image_s = ["korea.jpg",
 "salt lake.jpg",
 "utah.jpg",
-]
+"flowertemp.jpg"
+
+];
+let arrayimages=JSON.stringify(image_s);
+sessionStorage.setItem("myArray",arrayimages)
+let getimages=sessionStorage.getItem("myArray");
+let getArray=JSON.parse(getimages)
 
 
 //call the the class name where the images will show on html
 let currentimage = 0;
-let show_images = document.querySelector(".show_image")
 
+let number= document.querySelector(".show_image")
+
+function submit(){
+let slides =document.querySelector(".slides").value;
+image_s.push(slides);
+console.log(slides);
+localStorage.setItem("slides")
+}
 
 //variable to increment and decrement when the buttons are pressed
-let number= document.querySelector("show_image")
+
 //function for next buttton 
 function next(){
     currentimage ++;
     //access the array of pictures
-    show_images.src = image_s[currentimage]
+   
 
     //controll to not excede the array of images
     if(currentimage >= image_s.length ){
-        currentimage=0
+        currentimage = 0;
+
     }
-number.src=image_s[currentimage]
+    number.src=image_s[currentimage];
+
 }
 
-
-
 function previous(){
-    show_images.src = image_s[currentimage]
-
+    
     currentimage--
     if(currentimage < 0 ){
-        currentimage=image_s.length -1
+        currentimage = image_s.length-1;
     }
-    number.src =image_s[currentimage]
+    number.src = image_s[currentimage];
 }
