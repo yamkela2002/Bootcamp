686d5177374f48126802c04a5b89a57391d0532b
Yamkela Ngwetsheni <yamkela.ngwetsheni@gmail.com>
2023-04-17 16:10:47 +0200

    twelve commit

--- image slider/sliders.js
+++ image slider/sliders.js
@@ -1,53 +1,52 @@
 
-let show_images = document.querySelector(".show_image")
 
 //Array where pictures stored
-let image_s = ["korea.jpg",
-"salt lake.jpg",
-"utah.jpg",
-"flowertemp.jpg"
+let images;
+if (localStorage.getItem("images_s")) {
+  images = JSON.parse(localStorage.getItem("images_s"));
+} else {
+  images = ["korea.jpg", "salt lake.jpg", "utah.jpg", "flowertemp.jpg"];
+}
 
-];
-let arrayimages=JSON.stringify(image_s);
-sessionStorage.setItem("myArray",arrayimages)
-let getimages=sessionStorage.getItem("myArray");
-let getArray=JSON.parse(getimages)
-
-
+// let arrayimages=JSON.stringify(images);
+// sessionStorage.setItem("myArray",arrayimages)
+// let getimages=sessionStorage.getItem("myArray");
+// let getArray=JSON.parse(getimages)
+console.log(images)
 //call the the class name where the images will show on html
 let currentimage = 0;
 
-let number= document.querySelector(".show_image")
+let number = document.querySelector(".show_image");
+number.src = images[currentimage]
 
-function submit(){
-let slides =document.querySelector(".slides").value;
-image_s.push(slides);
-console.log(slides);
-localStorage.setItem("slides")
+function submit() {
+  let slides = document.querySelector(".slides").value;
+  console.log("Hello World")
+  console.log(slides);
+  images.push(slides);
+  
+  localStorage.setItem("images_s", JSON.stringify(images));
 }
 
 //variable to increment and decrement when the buttons are pressed
 
-//function for next buttton 
-function next(){
-    currentimage ++;
-    //access the array of pictures
-   
+//function for next buttton
+function next() {
+  currentimage++;
+  //access the array of pictures
 
-    //controll to not excede the array of images
-    if(currentimage >= image_s.length ){
-        currentimage = 0;
-
-    }
-    number.src=image_s[currentimage];
-
+  //controll to not excede the array of images
+  
+  if (currentimage >= images.length) {
+    currentimage = 0;
+  }
+  number.src = images[currentimage];
 }
 
-function previous(){
-    
-    currentimage--
-    if(currentimage < 0 ){
-        currentimage = image_s.length-1;
-    }
-    number.src = image_s[currentimage];
+function previous() {
+  currentimage--;
+  if (currentimage < 0) {
+    currentimage = images.length - 1;
+  }
+  number.src = images[currentimage];
 }
